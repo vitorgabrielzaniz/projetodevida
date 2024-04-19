66e8e4f28979ed95da80a7e988d51c6adb7cf71e
Vitor Gabriel Zaniz <108005361+vitorgabrielzaniz@users.noreply.github.com>
2024-04-19 10:30:02 -0300

    atualização dia 18

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo")
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i=0; i<botoes.length; i++)  {
-    botoes [i] .onclick= function()  {
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0; j<botoes,lenght; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
-        botoes[i].classList.add("ativo") 
-        tetos[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-07-05T20:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    return segundos;
+  }
